e3457648076ee8a31c300f0061abe08907a18757
hurrhnn <20sunrin022@sunrint.sen.hs.kr>
2023-11-26 08:51:19 +0900

    raplayer_jni: initiated native lib function

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,8 +3,60 @@
 
 using namespace std;
 
-extern "C" JNIEXPORT jstring JNICALL Java_xyz_hurrhnn_raplayer_1jni_MainActivity_stringFromJNI(JNIEnv *env,
+extern "C" {
+#include <raplayer.h>
+
+typedef struct {
+    JavaVM *g_vm;
+    jclass mainActivityClz;
+    jobject mainActivityObj, audioTrackObj;
+} jni_callback;
+
+JNIEXPORT jstring JNICALL Java_xyz_hurrhnn_raplayer_1jni_MainActivity_stringFromJNI(JNIEnv *env,
                                                                                     __attribute__((unused)) jobject obj) {
-    std::string hello = "Hello JNI World!";
+    string hello = "libraplayer version v";
+    hello.append(RAPLAYER_VERSION);
     return env->NewStringUTF(hello.c_str());
+}
+
+void client_frame_callback(void *frame, int frame_size, void *user_args) {
+    auto *jniCallbackCtx = static_cast<jni_callback *>(user_args);
+
+    JNIEnv *env;
+    jint res = (*jniCallbackCtx->g_vm).GetEnv((void **) &env, JNI_VERSION_1_6);
+    if (res != JNI_OK) {
+        res = (*jniCallbackCtx->g_vm).AttachCurrentThread(&env, nullptr);
+        if (JNI_OK != res) {
+            return;
+        }
+    }
+
+    frame_size *= (2 * 2); // 2 channels * 16 bit
+    jbyteArray jFrame = (*env).NewByteArray(frame_size);
+    (*env).SetByteArrayRegion(jFrame, 0, frame_size, static_cast<const jbyte *>(frame));
+
+    jmethodID mid = (*env).GetMethodID((*env).FindClass("xyz/hurrhnn/raplayer_jni/MainActivity"),
+                                       "bufferCallback",
+                                       "([BLandroid/media/AudioTrack;)V");
+    env->CallVoidMethod((*jniCallbackCtx).mainActivityObj, mid, jFrame,
+                        (*jniCallbackCtx).audioTrackObj);
+}
+
+JNIEXPORT jboolean JNICALL Java_xyz_hurrhnn_raplayer_1jni_MainActivity_startClientFromJNI(
+        JNIEnv *env,
+        jobject obj, jobject audio) {
+
+    int *status = new int;
+    *status = -1;
+
+    jni_callback jniCallbackCtx;
+    (*env).GetJavaVM(&jniCallbackCtx.g_vm);
+    jniCallbackCtx.mainActivityClz = (*env).GetObjectClass((*env).NewGlobalRef(obj));
+    jniCallbackCtx.mainActivityObj = (*env).NewGlobalRef(obj);
+    jniCallbackCtx.audioTrackObj = (*env).NewGlobalRef(audio);
+
+    char address[0x32] = "192.168.35.202";
+    ra_client(address, 3845, client_frame_callback, &jniCallbackCtx, status);
+    return true;
+}
 }
--- app/src/main/java/xyz/hurrhnn/raplayer_jni/MainActivity.java
+++ app/src/main/java/xyz/hurrhnn/raplayer_jni/MainActivity.java
@@ -1,6 +1,10 @@
 package xyz.hurrhnn.raplayer_jni;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,12 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     static {
+        System.loadLibrary("raplayer");
         System.loadLibrary("raplayer_jni");
+    }
+
+    private void bufferCallback(byte[] frame, AudioTrack audioTrack) {
+        audioTrack.write(frame, 0, frame.length);
     }
 
     @Override
@@ -17,8 +26,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.main_text);
+        Button startButton = findViewById(R.id.main_start_button);
+
         textView.setText(stringFromJNI());
+        startButton.setOnClickListener(v -> {
+            AudioTrack audio = new AudioTrack(AudioManager.STREAM_MUSIC,
+                48000, //sample rate
+                AudioFormat.CHANNEL_OUT_STEREO, // 2 channel
+                AudioFormat.ENCODING_PCM_16BIT, // 16-bit
+                960 * 2 * 2,
+                AudioTrack.MODE_STREAM);
+            audio.play();
+            class ClientThread extends Thread {
+                public void run() {
+                    startClientFromJNI(audio);
+                }
+            }
+            ClientThread clientThread = new ClientThread();
+            clientThread.start();
+        });
     }
 
     public native String stringFromJNI();
+
+    public native boolean startClientFromJNI(AudioTrack audio);
 }
