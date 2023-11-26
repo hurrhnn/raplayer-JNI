dd8eabc29a4978c181ccf2718a172349cfc2fd6b
hurrhnn <20sunrin022@sunrint.sen.hs.kr>
2023-11-26 10:10:02 +0900

    raplayer_jni: implemented default user interface

--- app/src/main/java/xyz/hurrhnn/raplayer_jni/MainActivity.java
+++ app/src/main/java/xyz/hurrhnn/raplayer_jni/MainActivity.java
@@ -1,13 +1,18 @@
 package xyz.hurrhnn.raplayer_jni;
 
+import android.graphics.Color;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Formatter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,28 +31,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.main_text);
+        TextView logView = findViewById(R.id.logging_text);
+        logView.setTextColor(Color.WHITE);
+
+        EditText addressEditText = findViewById(R.id.address_edittext);
+        EditText portEditText = findViewById(R.id.port_edittext);
         Button startButton = findViewById(R.id.main_start_button);
 
         textView.setText(stringFromJNI());
-        startButton.setOnClickListener(v -> {
-            AudioTrack audio = new AudioTrack(AudioManager.STREAM_MUSIC,
-                48000, //sample rate
-                AudioFormat.CHANNEL_OUT_STEREO, // 2 channel
-                AudioFormat.ENCODING_PCM_16BIT, // 16-bit
-                960 * 2 * 2,
-                AudioTrack.MODE_STREAM);
-            audio.play();
-            class ClientThread extends Thread {
-                public void run() {
-                    startClientFromJNI(audio);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            int count = 0;
+
+            @Override
+            public void onClick(View v) {
+                AudioTrack audio = new AudioTrack(AudioManager.STREAM_MUSIC, 48000, //sample rate
+                    AudioFormat.CHANNEL_OUT_STEREO, // 2 channel
+                    AudioFormat.ENCODING_PCM_16BIT, // 16-bit
+                    960 * 2 * 2, AudioTrack.MODE_STREAM);
+
+                audio.play();
+
+                String address = addressEditText.getText().toString();
+                String port = portEditText.getText().toString();
+
+                String text = logView.getText().toString() + " [" + new Formatter().format("%02d", ++count).toString() + "] Connecting to " + address + ":" + port + "...\n";
+                logView.setText(text);
+
+                class ClientThread extends Thread {
+                    private final String address;
+                    private final int port;
+
+                    public ClientThread(String address, int port) {
+                        this.address = address;
+                        this.port = port;
+                    }
+
+                    public void run() {
+                        startClientFromJNI(address, port, audio);
+                    }
                 }
+                ClientThread clientThread = new ClientThread(address, Integer.parseInt(port));
+                clientThread.start();
             }
-            ClientThread clientThread = new ClientThread();
-            clientThread.start();
         });
     }
 
     public native String stringFromJNI();
 
-    public native boolean startClientFromJNI(AudioTrack audio);
+    public native boolean startClientFromJNI(String address, int port, AudioTrack audio);
 }
