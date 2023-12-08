9425fd36ebc1ae96204f6d894614ce7811d08f94
Ur$obad <gottisttot4678@gmail.com>
2023-12-08 17:01:50 +0900

    Stats 기본 레이아웃 수정

--- app/src/main/java/xyz/hurrhnn/raplayer_jni/StatsActivity.java
+++ app/src/main/java/xyz/hurrhnn/raplayer_jni/StatsActivity.java
@@ -1,6 +1,8 @@
 package xyz.hurrhnn.raplayer_jni;
 
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +18,10 @@ static {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String userid = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
         StatsBinding statsbinding = StatsBinding.inflate(getLayoutInflater());
         setContentView(statsbinding.getRoot());
+        TextView title = statsbinding.statsTitle;
+        title.setText(String.format(title.getText().toString(), userid));
     }
 }
